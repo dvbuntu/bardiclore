fe62e95a65d9233f04cf7419f4e6812c48712032
Dan Van Boxel <the.dvb@gmail.com>
2017-04-04 19:10:21 -0400

    Transfer learning process

--- bardic.py
+++ bardic.py
@@ -129,7 +129,177 @@ bill.step(40,verbose=True)
 b2 = bard(model,diversity = .5, primer = ''.join(data[1,1000:1040]))
 b2.step(10)
 b2.step(1000)
-# seem ' ' is a fixed point at the moment
 
+# new simple model
+maxlen = 40
+model = Sequential()
+model.add(Embedding(len(chars), 48,input_length=maxlen))
+model.add(LSTM(64))
+model.add(Dense(len(chars)))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics = ['accuracy'])
+
+epochs = 5
+num_blocks = 5
+
+for j in tqdm(range(epochs)):
+    for b in tqdm(range(num_blocks)):
+        sentences = []
+        next_chars = []
+        for i in range(0, len(data[b]) - maxlen, step):
+                sentences.append(data[b,i: i + maxlen])
+                next_chars.append(data[b,i + maxlen])
+        # stick with dense encoding
+        X = np.zeros([len(sentences),maxlen],dtype=np.uint8)
+        # encode all in one-hot
+        Y = np.zeros([len(sentences),len(chars)],dtype=np.uint8)
+        i = 0
+        for t, char in enumerate(sentences[0]):
+            X[i, t]= char_indices[char]
+            Y[i, char_indices[next_chars[i]]] = 1
+        for i, sentence in enumerate(sentences[1:]):
+            X[i+1, :-1] = X[i, 1:]
+            X[i+1, -1] = char_indices[next_chars[i]]
+            Y[i+1, char_indices[next_chars[i+1]]] = 1
+        model.fit(X,Y,epochs=1,validation_split=0.1)
+    model.save_weights('bardicweights_simple_{0}.h5'.format(j))
+
+E, K1,R1,b1, Wf, bf= model.get_weights()
+
+model2 = Sequential()
+model2.add(Embedding(len(chars), 48,input_length=maxlen))
+model2.add(LSTM(64, return_sequences=True))
+model2.add(LSTM(64))
+model2.add(Dense(len(chars)))
+model2.add(Activation('softmax'))
+
+model2.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics = ['accuracy'])
+
+WW = model2.get_weights()
+WW[:4] = [E,K1,R1,b1]
+
+model2.set_weights(WW)
+
+epochs = 5
+num_blocks = 8
+
+for j in tqdm(range(epochs)):
+    for b in tqdm(range(num_blocks)):
+        sentences = []
+        next_chars = []
+        for i in range(0, len(data[b]) - maxlen, step):
+                sentences.append(data[b,i: i + maxlen])
+                next_chars.append(data[b,i + maxlen])
+        # stick with dense encoding
+        X = np.zeros([len(sentences),maxlen],dtype=np.uint8)
+        # encode all in one-hot
+        Y = np.zeros([len(sentences),len(chars)],dtype=np.uint8)
+        i = 0
+        for t, char in enumerate(sentences[0]):
+            X[i, t]= char_indices[char]
+            Y[i, char_indices[next_chars[i]]] = 1
+        for i, sentence in enumerate(sentences[1:]):
+            X[i+1, :-1] = X[i, 1:]
+            X[i+1, -1] = char_indices[next_chars[i]]
+            Y[i+1, char_indices[next_chars[i+1]]] = 1
+        model2.fit(X,Y,epochs=1,validation_split=0.1)
+    model2.save_weights('bardicweights_medium{0}.h5'.format(j))
+
+# full model
+WW2= model2.get_weights()
+
+model3 = Sequential()
+model3.add(Embedding(len(chars), 48,input_length=maxlen))
+model3.add(LSTM(64, return_sequences=True))
+model3.add(LSTM(64, return_sequences=True))
+model3.add(LSTM(64))
+model3.add(Dense(len(chars)))
+model3.add(Activation('softmax'))
+
+model3.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics = ['accuracy'])
+
+WW = model3.get_weights()
+WW[:7] = WW2[:7]
+
+model3.set_weights(WW)
+
+epochs = 3
+num_blocks = 10
+
+for j in tqdm(range(epochs)):
+    for b in tqdm(range(num_blocks)):
+        sentences = []
+        next_chars = []
+        for i in range(0, len(data[b]) - maxlen, step):
+                sentences.append(data[b,i: i + maxlen])
+                next_chars.append(data[b,i + maxlen])
+        # stick with dense encoding
+        X = np.zeros([len(sentences),maxlen],dtype=np.uint8)
+        # encode all in one-hot
+        Y = np.zeros([len(sentences),len(chars)],dtype=np.uint8)
+        i = 0
+        for t, char in enumerate(sentences[0]):
+            X[i, t]= char_indices[char]
+            Y[i, char_indices[next_chars[i]]] = 1
+        for i, sentence in enumerate(sentences[1:]):
+            X[i+1, :-1] = X[i, 1:]
+            X[i+1, -1] = char_indices[next_chars[i]]
+            Y[i+1, char_indices[next_chars[i+1]]] = 1
+        model3.fit(X,Y,epochs=1,validation_split=0.1)
+    model3.save_weights('bardicweights_large_{0}.h5'.format(j))
+
+
+# Alternate approach, wider net
+WW3 = model3.get_weights()
+
+model4 = Sequential()
+model4.add(Embedding(len(chars), 48,input_length=maxlen))
+model4.add(LSTM(128, return_sequences=True))
+model4.add(LSTM(64, return_sequences=True))
+model4.add(LSTM(64))
+model4.add(Dense(len(chars)))
+model4.add(Activation('softmax'))
+
+model4.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics = ['accuracy'])
+
+WW4 = model4.get_weights()
+# transfer layer 1 weights (and others)
+WW = model3.get_weights()
+WW[1:5] = WW4[1:5]
+WW[1][:,:128] = WW3[1][:,:128]
+WW[1][:,256:384] = WW3[1][:,128:]
+WW[2][:64,:128] = WW3[2][:,:128]
+WW[2][:64,256:384] = WW3[2][:,128:]
+WW[3][:128] = WW3[3][:128]
+WW[3][256:384] = WW3[3][128:]
+
+model4.set_weights(WW)
+
+
+epochs = 2
+num_blocks = 10
+
+for j in tqdm(range(epochs)):
+    for b in tqdm(range(num_blocks)):
+        sentences = []
+        next_chars = []
+        for i in range(0, len(data[b]) - maxlen, step):
+                sentences.append(data[b,i: i + maxlen])
+                next_chars.append(data[b,i + maxlen])
+        # stick with dense encoding
+        X = np.zeros([len(sentences),maxlen],dtype=np.uint8)
+        # encode all in one-hot
+        Y = np.zeros([len(sentences),len(chars)],dtype=np.uint8)
+        i = 0
+        for t, char in enumerate(sentences[0]):
+            X[i, t]= char_indices[char]
+            Y[i, char_indices[next_chars[i]]] = 1
+        for i, sentence in enumerate(sentences[1:]):
+            X[i+1, :-1] = X[i, 1:]
+            X[i+1, -1] = char_indices[next_chars[i]]
+            Y[i+1, char_indices[next_chars[i+1]]] = 1
+        model4.fit(X,Y,epochs=1,validation_split=0.1)
+    model4.save_weights('bardicweights_large_{0}.h5'.format(j))
 
 
